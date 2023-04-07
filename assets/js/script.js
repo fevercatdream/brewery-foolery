85e337befcf508c85de9cc02b2e4e0d84dced959
RyanWood2334 <ryanwooduwcoding.2334@gmail.com>
2023-04-07 13:21:49 -0700

    fetch API

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,18 +1,30 @@
-var modalEl = document.querySelector(".modal");
-var modalCloseBtn = document.querySelector("#modal-close");
-var cardEls = document.querySelectorAll(".card");
+// var modalEl = document.querySelector(".modal");
+// var modalCloseBtn = document.querySelector("#modal-close");
+// var cardEls = document.querySelectorAll(".card");
 
-for (var i = 0; i < cardEls.length; i++) {
-    var cardEl = cardEls[i];
-    cardEl.addEventListener("click", openModal);   
-}
+// for (var i = 0; i < cardEls.length; i++) {
+//   var cardEl = cardEls[i];
+//   cardEl.addEventListener("click", openModal);
+// }
 
-modalCloseBtn.addEventListener("click", closeModal);
+// modalCloseBtn.addEventListener("click", closeModal);
 
-function openModal(){
-    modalEl.classList.add("is-active");
-}
+// function openModal() {
+//   modalEl.classList.add("is-active");
+// }
 
-function closeModal(){
-    modalEl.classList.remove("is-active");
-}
+// function closeModal() {
+//   modalEl.classList.remove("is-active");
+// }
+
+fetch(
+  "https://trefle.io/api/v1/plants?token=i8xXdNgREiXkmggN1WK7Pjg-CP0dc4uTU3-mRBO6oKY",
+  {
+    method: "GET",
+    headers: {
+      "Content-Type": "application/json",
+    },
+  }
+).then(function (response) {
+  console.log(response);
+});
