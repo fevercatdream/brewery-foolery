79684e76727eaea198aaa11930642d9c90a2aa28
RyanWood2334 <ryanwooduwcoding.2334@gmail.com>
2023-04-10 15:09:55 -0700

    added pseudocoding for a portion of js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,49 +1,38 @@
-// var modalEl = document.querySelector(".modal");
-// var modalCloseBtn = document.querySelector("#modal-close");
-// var cardEls = document.querySelectorAll(".card");
+var modalEl = document.querySelector(".modal");
+var modalCloseBtn = document.querySelector("#modal-close");
+var cardEls = document.querySelectorAll(".card");
 
-// for (var i = 0; i < cardEls.length; i++) {
-//   var cardEl = cardEls[i];
-//   cardEl.addEventListener("click", openModal);
-// }
-
-// modalCloseBtn.addEventListener("click", closeModal);
-
-// function openModal() {
-//   modalEl.classList.add("is-active");
-// }
-
-// function closeModal() {
-//   modalEl.classList.remove("is-active");
-// }
-
-//function for querying brewery API
-//      needs to fetch from 3 different criteria
-//      fetch function for state
-//      fetch function for city
-//      fetch function for zip code
-function triviaFetch() {
-  fetch("https://api.openbrewerydb.org/v1/breweries?per_page=10")
-    .then(function (response) {
-      return response.json();
-    })
-    .then(function (data) {
-      console.log(data);
-      return data;
-    });
+for (var i = 0; i < cardEls.length; i++) {
+  var cardEl = cardEls[i];
+  cardEl.addEventListener("click", openModal);
 }
 
-// function
+modalCloseBtn.addEventListener("click", closeModal);
+
+function openModal() {
+  modalEl.classList.add("is-active");
+}
+
+function closeModal() {
+  modalEl.classList.remove("is-active");
+}
 
 //define variables for each element being modified
 //
 //
 //
-//
+//function?(start ith just getting the fetch command and changing it for each criteria) for querying brewery API
+// ----
+//      needs to fetch from 3 different criteria (for loop?)
+//      fetch function for state
+//      fetch function for city
+//      fetch function for zip code
+//      for loop/if else(?): needs to alter fetch url for the input given i.e "which state, city or zip code"
 //
 //
 //
 //function for rendering the search criteria
+// -----
 // needs to get out of array
 // create and append a card for each search result based on an index and where values are in that array
 //      name
@@ -51,32 +40,46 @@ function triviaFetch() {
 //
 //
 //function for iterating each search result and rendering it to page
+// -----
 //      create for loop for getting each object out of the array
 //      modify element in card header/body
 //      add event handler for prompting modal
 //      needs to sort the results somehow
 //
 // render on modal other info
+// -----
 //      city
 //      state
 //      website url
 //      phone number
 //      type of brewery
+//      map?**
+//
+//create map for modal
+//----
+//      explore google maps api for how to embed
+//      get lat/long keys and values from fetch return objects
+//      input into map javascript function
+//      https://developers.google.com/maps here's a helpful URL
 
 //function for modifying modal text and rendering it
+// ------
 //      needs to access fetched array
 //      needs to index that array for correct info
 //      needs to modify elements in modal to display text of selected values
-//      ****bonus - have an event listener on a button that targets the next       brewery modal
+//      ****bonus - have an event listener on a button that targets the next
 //
 //function for fetching trivia questions
+// -----
 //      fetches trivia questions based on user input from selector drop menu
+//      get appropriate keys and values from return array (question, incorrect answer, correct)
 //      stores the questions in variable
 //      fetches answers (all answers, concat the incorrect and correct answers together)
 //      stores that new array in variable
 //      fetch correct answer in it's own variable
 //
 //function for rendering the question/answers to page
+// ------
 //      grab the variables from trivia fetch function
 //      add event handler on 2 buttons: one button renders question and concatted answer array
 //      other button renders correct answer below it and adds a subtle animation to click first button to generate new question
